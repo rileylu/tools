085d0e97061448afad52f7b5f0a8f6554fd62f8b
riley <rileylu@yeah.net>
2017-05-25 21:11:32 +0800

    udpate

--- thread_pool/thread_pool/main.cpp
+++ thread_pool/thread_pool/main.cpp
@@ -3,18 +3,27 @@
 #include <thread>
 
 using namespace std;
+std::mutex x;
 
 void foo()
 {
-    for(int i=0;i<10;++i)
+	for (int i = 0; i < 100; ++i)
+	{
+		std::lock_guard<std::mutex> lk(x);
         std::cout<<"Thread "<<std::this_thread::get_id()<<" no.:"<<i<<std::endl;
+	}
 }
 
 int main(int argc, char *argv[])
 {
 
     thread_pool tp;
-    for(unsigned i=0;i<10;++i)
-        tp.submit(foo);
+	std::vector<std::future<void>> res;
+	for (unsigned i = 0; i < 10; ++i)
+	{
+		res.push_back(std::move(tp.submit(foo)));
+	}
+	for (auto it = res.begin(); it != res.end(); ++it)
+		it->get();
     return 0;
 }
